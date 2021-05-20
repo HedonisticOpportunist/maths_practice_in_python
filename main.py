7cd451e555e00967b5079a66b2be2821eebafe00
HedonisticOpportunist <fujitahibaraki@gmail.com>
2021-05-20 22:34:19 +0100

    uploaded sequencesAndSeries file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numberSystems
+import sequencesAndSeries
 
 # binary to decimal
 print(numberSystems.convert_into_decimal("101010", 2))
@@ -17,3 +18,12 @@ print(numberSystems.convert_hex_into_decimal("F9B3"))
 
 # decimal to hexadecimal
 print(numberSystems.convert_decimal_to_hexadecimal(14397))
+
+# find the nth term in an arithmetic sequence
+print(sequencesAndSeries.find_nth_term_for_an_arithmetic_sequence(3, 5, 10))
+
+# find the nth term in a geometric sequence
+print(sequencesAndSeries.find_the_nth_term_for_a_geometric_sequence(2, 3, 7))
+
+# find the sum of an arithmetic sequence
+print(sequencesAndSeries.find_sum_of_arthimetic_sequence(10, 3, 4))
---
+++ sequencesAndSeries.py
@@ -0,0 +1,30 @@
+
+def find_nth_term_for_an_arithmetic_sequence(first_term: int, common_difference: int, requested_term: int) -> int:
+    """
+    Find the nth term in an arthimetic sequence
+    :param first_term:
+    :param common_difference:
+    :param requested_term
+    :return: the nth term
+    """
+    return first_term + (requested_term - 1) * common_difference
+
+def find_the_nth_term_for_a_geometric_sequence(first_term: int, common_ratio: int, requested_term: int) -> int:
+    """
+    Find the nth term in a geometric sequence
+    :param first_term:
+    :param common_ratio:
+    :param requested_term:
+    :return: the nth term
+    """
+    return first_term * common_ratio**(requested_term - 1)
+
+def find_sum_of_arthimetic_sequence(requested_terms: int, first_term: int, common_difference: int) -> int:
+    """
+    Finds the sum of an arthimetic sequence
+    :param requested_terms:
+    :param first_term:
+    :param common_difference:
+    :return: the sum of an arthimetic sequence
+    """
+    return int((requested_terms/2) * (((2 * first_term) + ((requested_terms - 1) * common_difference))))
